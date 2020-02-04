e337d0b31b1dd155f252f5de6757d50824da5802
Kevin Clark <kevin.clark93@gmail.com>
2020-02-04 23:20:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,7 +8,8 @@ def directors_totals(nds)
     movie_index = 0
     
     while movie_index < directors_database[director_index][:movies].length do
-      \
+      
+      movie_index += 1
     end
     director_index += 1
   end
