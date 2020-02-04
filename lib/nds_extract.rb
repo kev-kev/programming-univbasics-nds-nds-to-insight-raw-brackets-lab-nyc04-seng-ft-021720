c65d628b7477d419d05d22c65d4a25b764136b49
Kevin Clark <kevin.clark93@gmail.com>
2020-02-04 22:04:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,20 +19,6 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  total = 0
-  director_index = 0
-  while (director_index < directors_database.length) do
-    movie_index = 0
-    while (movie_index < directors_database[director_index][:movies].length) do
-      total +=  directors_database[director_index][:movies][movie_index][:worldwide_gross]
-      movie_index += 1
-    end
-    director_index += 1
-  end
-  return total
 end
 
-# First number is director_index, second is movie_index
-# pp directors_database[0][:movies][0][:worldwide_gross]
-# Need to iterate through directors and iterate through each of their movies, summing the worldwide gross
-# While the director_index is less than the length of the directors_database...
+pp directors_database[0]
