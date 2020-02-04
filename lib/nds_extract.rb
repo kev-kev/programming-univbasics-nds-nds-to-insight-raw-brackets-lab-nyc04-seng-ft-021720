a47be60cf7c661aafc6802b90f51d03beb5081a0
Kevin Clark <kevin.clark93@gmail.com>
2020-02-04 23:20:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,11 +3,15 @@ require 'directors_database'
 require 'pp'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+  director_index = 0
+  while director_index < directors_database.length do
+    movie_index = 0
+    
+    while movie_index < directors_database[director_index][:movies].length do
+      \
+    end
+    director_index += 1
+  end
   result = {
     
   }
